de4aa0e3342164f66206ecc3774f8176d5230dde
oluwatunbi <davies899@yahoo.com>
2020-08-25 19:07:23 +0100

    Updates

---
+++ precache-manifest.417742d16464f6f9c9a2204774992cdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24d579e804af19e90ca6542b61c1988f",
+    "url": "/Face-recognition-app/index.html"
+  },
+  {
+    "revision": "b8a2b40e1c9ad38ccec0",
+    "url": "/Face-recognition-app/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "d5e5f7938aefcf60ef51",
+    "url": "/Face-recognition-app/static/css/main.f83850e9.chunk.css"
+  },
+  {
+    "revision": "b8a2b40e1c9ad38ccec0",
+    "url": "/Face-recognition-app/static/js/2.aa96c117.chunk.js"
+  },
+  {
+    "revision": "d5e5f7938aefcf60ef51",
+    "url": "/Face-recognition-app/static/js/main.9714c5c3.chunk.js"
+  },
+  {
+    "revision": "c96c9418a63695347185",
+    "url": "/Face-recognition-app/static/js/runtime~main.afa7066a.js"
+  }
+]);
